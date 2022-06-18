9038822829ae4fe6cdad5d30b001a43f0d2d4a03
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-18 14:39:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenecTypesAndValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             types divide into structures and classes:
+
+            structures = Value Types
+                *primitive types
+                *custom types
+            
+            classes = Reference Types
+                *arrays
+                *strings
+                *custom classes
+
+
+            Vale Types                              Reference Types
+            Structures                              Classes
+            
+            *allocated on stack (type of memory)    *you need to allocate memory (new operator)
+            *memory allocation done automatically   *memory allocated on heap (type of memory)
+            *immediately removed when out of scope  *garbage collector by CLR
+            
+            
+            
+            objects when removed out of scope continue to exist on the heap
+            
+             */
+        }
+    }
+}
